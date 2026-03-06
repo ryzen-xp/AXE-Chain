11823dedf3c09602c0592b716ea41384c9dd455d
ryzen_xp <iron4540@gmail.com>
2026-03-06 20:11:40 +0530

    [Setup] : Folder tree setup

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,5 @@
-pub mod block;
-pub mod blockchain;
-use crate::blockchain::Blockchain;
-
 fn main() {
-    let mut blockchain = Blockchain::new();
-
-    println!("Mining block 1...");
-    blockchain.add_block("Alice pays Bob 10 BTC".to_string());
-
-    println!("Mining block 2...");
-    blockchain.add_block("Bob pays Charlie 5 BTC".to_string());
-
-    for block in &blockchain.chain {
-        println!("{:?}", block);
-    }
-
-    println!("Is blockchain valid? {}", blockchain.is_valid());
+    println!("Running Chain  Loading !!!!");
 }
+
+
